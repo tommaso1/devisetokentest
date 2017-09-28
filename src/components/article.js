3edaf7205534fec07c47bcf9c84dfda105c41d44
tommaso <tommasorosso1@gmail.com>
2017-09-28 11:44:58 +0200

    incluso font awesome

--- src/components/article.js
+++ src/components/article.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
 
 const Article = ({title, body, id, current_user, article_author_id, destroyArticle}) => {
-    return (
-        <div className="article">
-            <div className="article-title">
-                <h3>
-                    {title}
-                </h3>
-            </div>
-            <div className="article-body">
-                <p>{body}</p>
-            </div>
-            {(current_user.role === 'admin' || current_user.id !== null && current_user.id === article_author_id) &&
-                <div className="article-button" >
-                    <button   className="btn btn-danger article-button" onClick={() => {destroyArticle(id)}}>Cancella</button>
-                </div>
-            }
-        </div>
-    )
+  return (
+    <div className="article">
+      <div className="article-title">
+        <h3>
+          {title}
+        </h3>
+      </div>
+      <div className="article-body">
+
+        <p>{body}</p>
+      </div>
+      {(current_user.role === 'admin' || current_user.id !== null && current_user.id === article_author_id) &&
+      <div className="article-button" >
+
+        <button   className="btn btn-danger article-button" onClick={() => {destroyArticle(id)}}><i className="fa fa-trash-o"/> Cancella</button>
+      </div>
+      }
+    </div>
+  )
 };
 
 export default Article
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import Index from './components/Index';
 import CreateArticle from './containers/CreateArticle';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 import './index.css';
 
 const loggerMiddleware = createLogger();
